15f9e3b1baa1f8d5f978b97a6424d2ee96f3d296
huynguyen1212 <huyro1212000@gmail.com>
2022-05-30 13:38:10 +0700

    authentication gg

--- nodejs_authentication-gg/index.js
+++ nodejs_authentication-gg/index.js
@@ -1,32 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const csrf = require('csurf');
-const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
-const MemoryStore = require('memorystore')(expressSession)
-const passport = require('passport');
-const flash = require('connect-flash');
+const express = require("express");
+const mongoose = require("mongoose");
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const MemoryStore = require("memorystore")(expressSession);
+const passport = require("passport");
+const flash = require("connect-flash");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views',);
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 app.use(express.urlencoded({ extended: true }));
 
-const mongoURI = require('./config/monkoKEY');
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, },).then(() => console.log("Connected !"),);
+//connect mongodb
+const mongoURI = require("./config/monkoKEY");
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connected !"));
 
-app.use(cookieParser('random'));
+app.use(cookieParser("random"));
 
-app.use(expressSession({
+app.use(
+  expressSession({
     secret: "random",
     resave: true,
     saveUninitialized: true,
     // setting the max age to longer duration
     maxAge: 24 * 60 * 60 * 1000,
     store: new MemoryStore(),
-}));
+  })
+);
 
 app.use(csrf());
 app.use(passport.initialize());
@@ -35,13 +45,13 @@ app.use(passport.session());
 app.use(flash());
 
 app.use(function (req, res, next) {
-    res.locals.success_messages = req.flash('success_messages');
-    res.locals.error_messages = req.flash('error_messages');
-    res.locals.error = req.flash('error');
-    next();
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.error_messages = req.flash("error_messages");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-app.use(require('./controller/routes.js'));
+app.use(require("./controller/routes.js"));
 
 const PORT = process.env.PORT || 8000;
 
