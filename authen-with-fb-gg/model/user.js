b31f35402f97343cf7581624590a5e67fc9bd9a1
huynguyen1212 <huyro1212000@gmail.com>
2022-06-01 11:28:53 +0700

    authentication fb

--- authen-with-fb-gg/model/user.js
+++ authen-with-fb-gg/model/user.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
     required: true,
   },
   email: {
     type: String,
-    // required: true,
   },
   password: {
     type: String,
